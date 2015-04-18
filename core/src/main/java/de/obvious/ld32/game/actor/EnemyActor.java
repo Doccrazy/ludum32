904e977bdc2e668d059e973f8c2c48d292a78df9
mtiffi <tiffi1989@gmail.com>
2015-04-19 00:54:49 +0200

    Weapon drops and Abilities

--- core/src/main/java/de/obvious/ld32/game/actor/EnemyActor.java
+++ core/src/main/java/de/obvious/ld32/game/actor/EnemyActor.java
@@ -9,10 +9,11 @@ import com.badlogic.gdx.physics.box2d.Body;
 
 import de.obvious.ld32.core.Resource;
 import de.obvious.ld32.game.abilities.Ability;
+import de.obvious.ld32.game.abilities.InsectAbility;
+import de.obvious.ld32.game.world.GameWorld;
 import de.obvious.shared.game.actor.ShapeActor;
 import de.obvious.shared.game.base.CollisionListener;
 import de.obvious.shared.game.world.BodyBuilder;
-import de.obvious.shared.game.world.Box2dWorld;
 import de.obvious.shared.game.world.ShapeBuilder;
 
 public class EnemyActor extends ShapeActor implements CollisionListener{
@@ -23,20 +24,24 @@ public class EnemyActor extends ShapeActor implements CollisionListener{
 
     private Map<Integer, Ability> abilities = new HashMap<Integer, Ability>();
 
-	public EnemyActor(Box2dWorld world, Vector2 spawn, boolean spawnIsLeftBottom) {
+	public EnemyActor(GameWorld world, Vector2 spawn, boolean spawnIsLeftBottom) {
 		super(world, spawn, spawnIsLeftBottom);
+		abilities.put(0, new InsectAbility(world));
 
 	}
 
 	@Override
 	protected BodyBuilder createBody(Vector2 spawn) {
-		return BodyBuilder.forDynamic(spawn).fixShape(ShapeBuilder.circle(RADIUS));
+		return BodyBuilder.forStatic(spawn).fixShape(ShapeBuilder.circle(RADIUS));
 	}
 
 	@Override
 	protected void doAct(float delta) {
 		super.doAct(delta);
+
+
 		if(lives <= 0){
+			((GameWorld) world).getPlayer().setAbility(1, abilities.get(0));
 			kill();
 		}
 	}
@@ -44,7 +49,7 @@ public class EnemyActor extends ShapeActor implements CollisionListener{
 	@Override
 	public void draw(com.badlogic.gdx.graphics.g2d.Batch batch, float parentAlpha) {
 		batch.draw(Resource.GFX.insect, getX(), getY(), getWidth(), getHeight()*1.5f);
-		batch.draw(Resource.GFX.lifeBar,getX(), getY() + 1.5f, getWidth() * lives/100f, getWidth()/10f);
+		batch.draw(Resource.GFX.lifeBar,getX(), getY() + 1.6f, getWidth() * lives/100f, getWidth()/10f);
 
 	}
 
