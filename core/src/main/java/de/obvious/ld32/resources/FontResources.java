3fb8b68b9de20194aeb12f56834f142557b1821d
Matthias Piepkorn <mpiepk@gmail.com>
2015-04-20 23:02:07 +0200

    insult generator

--- core/src/main/java/de/obvious/ld32/resources/FontResources.java
+++ core/src/main/java/de/obvious/ld32/resources/FontResources.java
@@ -1,10 +1,18 @@
 package de.obvious.ld32.resources;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.MathUtils;
 
 import de.obvious.shared.core.ResourcesBase;
 
 public class FontResources extends ResourcesBase {
 	public BitmapFont retro = bitmapFont("Font/font");
 	public BitmapFont retroSmall = bitmapFont("Font/fontSmall");
+
+	public String[] insults = Gdx.files.internal("insults.txt").readString().split("\r?\n");
+
+	public String getRandomInsult() {
+	    return insults[MathUtils.random(insults.length-1)];
+	}
 }
